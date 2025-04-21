e841156cfa32d24872db99f822de632be104f251
baedj <qoehdwns>
2025-04-21 18:02:00 +0900

    nice

--- src/main/java/com/peakmeshop/api/mapper/WishlistMapper.java
+++ src/main/java/com/peakmeshop/api/mapper/WishlistMapper.java
@@ -5,19 +5,28 @@ import com.peakmeshop.domain.entity.Wishlist;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {MemberMapper.class, WishlistItemMapper.class})
+@Mapper(componentModel = "spring", uses = {MemberMapper.class, ProductMapper.class})
 public interface WishlistMapper {
     
+    @Mapping(source = "member.id", target = "memberId")
+    @Mapping(source = "product.id", target = "productId")
     WishlistDTO toDto(Wishlist wishlist);
+    
+    List<WishlistDTO> toDtoList(List<Wishlist> wishlists);
     
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(source = "memberId", target = "member.id")
+    @Mapping(source = "productId", target = "product.id")
     Wishlist toEntity(WishlistDTO wishlistDTO);
     
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(source = "memberId", target = "member.id")
+    @Mapping(source = "productId", target = "product.id")
     void updateWishlistFromDto(WishlistDTO wishlistDTO, @MappingTarget Wishlist wishlist);
 } 
